0859e866816224cbc5d346df6c6428b54ca536b7
GuyOhm <myw4tch@gmail.com>
2019-09-09 10:22:12 +0200

    modify generateGrid() to draw the board

--- js/class/board.js
+++ js/class/board.js
@@ -6,26 +6,45 @@ import Square from './square.js';
 export default class Board {
     constructor(size) {
         this.size = size;
-        this.squares = [];
-        this.generateGrid(size);
+        this.squares = this.generateGrid(size);
     }
 
     /*
-    * This function generates a list of coordinates based on the size
+    * This function generates a a grid and an array of Squares based on the size
     * @param {integer} size - Size of the board
+    * @return {Square[]} squares - Array of Squares
     */ 
     generateGrid(size) {
-        
+        const squares = [];
+        const body = document.querySelector('body');
+        const screenSize = (window.innerWidth < window.innerHeight) ? window.innerWidth : window.innerHeight;
+        // Add a row to the page
         for(let i = 0; i < size; i++) {
+            const rowDiv = document.createElement('div');
+            rowDiv.classList.add('row');
+            rowDiv.id = i;
+            rowDiv.style.height = screenSize / size + 'px';
+            body.appendChild(rowDiv);
+            // Add columns to the row
             for(let j = 0; j < size; j++) {
                 const row = i;
                 const col = j;
-                this.squares.push([row, col]);
+                squares.push(new Square(row, col, null));
+                const colDiv = document.createElement('div');
+                colDiv.classList.add('col');
+                colDiv.id = j;
+                rowDiv.appendChild(colDiv);
+                colDiv.style.width = screenSize / size + 'px';
             }
         }
+        return squares;
     }
+
+    /*
+    *
+    */
     getSquare(row, col) {
         // check if square exists, if not return null
-        return this.squares[col * this.size + row]
+        return this.squares[row * this.size + col];
     }
 }
