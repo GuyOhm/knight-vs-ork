64543dd4d442a69b9da3ef9f6c00917f9fbff855
GuyOhm <myw4tch@gmail.com>
2019-09-17 10:59:01 +0200

    Modify generateGrid(): don't need to use selector just to reference the div of the square obj Move and optimise (with refresh()) placement methods to class Board

--- js/class/board.js
+++ js/class/board.js
@@ -8,9 +8,9 @@ export default class Board {
         this.size = size;
         this.squares = this.generateGrid(size);
     }
-
+    
     /*
-    * This function generates a a grid and an array of Squares based on the size
+    * This function generates a grid and an array of Squares based on the size
     * @param {integer} size - Size of the board
     * @return {Square[]} squares - Array of Squares
     */ 
@@ -21,28 +21,60 @@ export default class Board {
         for(let i = 0; i < size; i++) {
             const rowDiv = document.createElement('div');
             rowDiv.classList.add('row');
-            rowDiv.id = i;
             body.appendChild(rowDiv);
             // Add columns to the row
             for(let j = 0; j < size; j++) {
                 const row = i;
                 const col = j;
-                const div = `.row[id='${row}'] .col[id='${col}']`;
+                const div = document.createElement('div');
+                div.classList.add('col');
                 squares.push(new Square(row, col, div));
-                const colDiv = document.createElement('div');
-                colDiv.classList.add('col');
-                colDiv.id = j;
-                rowDiv.appendChild(colDiv);
+                rowDiv.appendChild(div);
             }
         }
         return squares;
     }
-
+    
     /*
     *
     */
     getSquare(row, col) {
-        // check if square exists, if not return null
+        // TODO: check if square exists, if not return null
         return this.squares[row * this.size + col];
+    }
+    
+    // This function randomly picks an element of an array and pops it
+    // @param {integer[]} array
+    // @return {integer} elt - the randomly picked element
+    pickAndPop(array) {
+        const randomIdx = Math.floor(Math.random() * array.length);
+        const elt = array[randomIdx];
+        array.splice(randomIdx, 1);
+        return elt;
+    }
+    
+    placePlayers(players, squaresCopy) {
+        // Place randomly 2 players on the board
+        for (let player of players) {
+            const square = this.pickAndPop(squaresCopy);
+            square.player = player;
+            square.refresh();
+        }
+    }
+    
+    placeWeapons(weapons, squaresCopy) {
+        for (let weapon of weapons) {
+            const square = this.pickAndPop(squaresCopy);
+            square.weapon = weapon;
+            square.refresh();
+        }
+    }
+    
+    placeWalls(nbOfWalls, squaresCopy) {
+        for (let i = 0; i < nbOfWalls; i++) {
+            const square = this.pickAndPop(squaresCopy);
+            square.isWall = true;
+            square.refresh();
+        }
     }
 }
