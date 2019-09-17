0141aea6fdc8c01733423137327b8d686bd0f997
GuyOhm <myw4tch@gmail.com>
2019-09-17 11:10:02 +0200

    Create a Game class as a controller to create the game

---
+++ js/class/game.js
@@ -0,0 +1,27 @@
+import Weapon from './weapon.js';
+import Player from './player.js';
+import Board from './board.js';
+
+export default class Game {
+    constructor () {
+        this.board = new Board(10);
+        this.players = [
+            new Player(null, 'player1'),
+            new Player(null, 'player2')
+        ];
+        this.weapons = [
+            new Weapon('Katana', 13, 'katana'),
+            new Weapon('Knife', 10, 'knife'),
+            new Weapon('Chainsaw', 17, 'chainsaw'),
+            new Weapon('Axe', 15, 'axe')
+        ];
+        this.nbOfWalls = 12;
+    }
+
+    init() {
+        const squaresCopy = this.board.squares.slice(0);
+        this.board.placePlayers(this.players, squaresCopy);
+        this.board.placeWeapons(this.weapons, squaresCopy);
+        this.board.placeWalls(this.nbOfWalls, squaresCopy);
+    }
+}
